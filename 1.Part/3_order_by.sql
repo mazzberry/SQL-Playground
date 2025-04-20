7db03987063d94a43dc36b12c75ef6a8342aa07d
Mazzberry <h.mohammadreza736@gmail.com>
2025-04-20 11:51:33 +0330

    Add SQL query to retrieve customers ordered by first and last name

---
+++ 1.Part/3_order_by.sql
@@ -0,0 +1,12 @@
+#SELECT * 
+#FROM 
+#	products 
+#ORDER BY unit_price DESC ;
+
+SELECT *
+FROM customers 
+ORDER BY first_name, last_name
+
+#ORDER BY __ DESK MEANS "NOZOULI"
+#ORFER BY __ ACS MEANS "SOOUDI"
+#ORDER BY __ NAME(a string attribute) "IT WILL SORT ALPHABETICAL"

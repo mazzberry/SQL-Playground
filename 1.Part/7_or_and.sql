2011632dc9ad4602c1ec46819cda70e15bd4d272
Mazzberry <h.mohammadreza736@gmail.com>
2025-04-21 09:43:44 +0330

    Refactor SQL comments and retain query for customers with specific conditions

--- 1.Part/7_or_and.sql
+++ 1.Part/7_or_and.sql
@@ -1,5 +1,20 @@
-SELECT *
-FROM customers
-WHERE
-    date_of_birth > '2000-01-01' AND
-    province != 'isfahan'
+-- / and / 
+
+-- SELECT *
+-- FROM customers
+-- WHERE
+--     date_of_birth > '2000-01-01' AND
+--     province != 'isfahan'
+
+-- -- / and /
+-- SELECT * FROM customers
+-- WHERE province != 'isfahan' AND
+-- date_of_birth < '2000-01-01'
+
+SELECT * FROM customers
+WHERE province = 'isfahan' OR date_of_birth < '2000-01-01' AND
+first_name =  'Ali'
+
+
+-- sql aval AND ha ro mohasebe mikone bad OR ha ro
+-- agar olaviat digari darim bayad az () parantes estefade konim

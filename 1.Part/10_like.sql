d235764ecd411b655d734dba0f21c20295a0494d
Mazzberry <h.mohammadreza736@gmail.com>
2025-04-21 22:29:04 +0330

    Add SQL query to select products with name pattern matching

---
+++ 1.Part/10_like.sql
@@ -0,0 +1,7 @@
+SELECT *  
+FROM products
+WHERE name LIKE "c%%z" -- "char%"(shoroo) va "%char"(payan) kari mikone 
+--ke ba characteri ke mikhaim shoromishe va tamoom mishe ro peyda konim
+
+-- LIKE  daroon chizi ke mikhaym begardim ro barasi mikone ta 
+-- ba deghat bishtari begarde va peyda kone 

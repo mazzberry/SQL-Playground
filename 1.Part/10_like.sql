c6efb059128610dfae5dd21a3550e0d008ec1684
Mazzberry <h.mohammadreza736@gmail.com>
2025-04-22 10:20:40 +0330

    Refactor SQL comments for clarity and organization

--- 1.Part/10_like.sql
+++ 1.Part/10_like.sql
@@ -1,8 +1,9 @@
 -- SELECT *  
 -- FROM products
--- WHERE name LIKE "c%%z" -- "char%"(shoroo) va "%char"(payan) kari mikone 
---ke ba characteri ke mikhaim shoromishe va tamoom mishe ro peyda konim
+-- WHERE name LIKE "c%%z"
 
+-- "char%"(shoroo) va "%char"(payan) kari mikone 
+--ke ba characteri ke mikhaim shoromishe va tamoom mishe ro peyda konim
 -- LIKE  daroon chizi ke mikhaym begardim ro barasi mikone ta 
 -- ba deghat bishtari begarde va peyda kone 
 

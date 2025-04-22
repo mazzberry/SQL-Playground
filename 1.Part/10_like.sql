1d7cb7ce7a3ee65e5f92132f81d5512ccf680967
Mazzberry <h.mohammadreza736@gmail.com>
2025-04-22 10:16:21 +0330

    Refactor SQL comments and update query to filter products by name pattern

--- 1.Part/10_like.sql
+++ 1.Part/10_like.sql
@@ -1,7 +1,14 @@
-SELECT *  
-FROM products
-WHERE name LIKE "c%%z" -- "char%"(shoroo) va "%char"(payan) kari mikone 
+-- SELECT *  
+-- FROM products
+-- WHERE name LIKE "c%%z" -- "char%"(shoroo) va "%char"(payan) kari mikone 
 --ke ba characteri ke mikhaim shoromishe va tamoom mishe ro peyda konim
 
 -- LIKE  daroon chizi ke mikhaym begardim ro barasi mikone ta 
 -- ba deghat bishtari begarde va peyda kone 
+
+SELECT * 
+FROM products
+WHERE name LIKE "%ee_%"
+
+-- "%ee_%" yani har chizi ke mikhad bashe badesh "ee" bashe va _ underscore yani 1 character bashe va % akhar ham
+-- yani har chizi ke mikhad bashe vali vajebe ke bashe 

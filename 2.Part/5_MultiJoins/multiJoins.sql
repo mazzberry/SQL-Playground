4bb3d9ccd6a8359d7e50ebaf2677083023f0388b
Mazzberry <h.mohammadreza736@gmail.com>
2025-04-27 00:03:30 +0330

    Refactor SQL query to select specific columns from comments, products, and customers

--- 2.Part/5_MultiJoins/multiJoins.sql
+++ 2.Part/5_MultiJoins/multiJoins.sql
@@ -1,4 +1,4 @@
-SELECT *
+SELECT p.name, cu.first_name, cu.last_name, c.body
 FROM store.comments c
 JOIN products p  ON c.product_id = p.product_id
 JOIN customers cu ON c.customer_id = cu.customer_id

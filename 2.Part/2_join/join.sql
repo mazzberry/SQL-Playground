6d553c11db89ede6f335b47715373274b2a4740b
Mazzberry <h.mohammadreza736@gmail.com>
2025-04-26 21:35:08 +0330

    Refactor join SQL query to select specific order and customer details

--- 2.Part/2_join/join.sql
+++ 2.Part/2_join/join.sql
@@ -1,3 +1,9 @@
 USE store;
+-- SELECT *
+-- FROM orders
+-- join customers ON orders.customer_id = customers.customer_id
 
-SELECT * FROM store.order_items;
+SELECT o.order_id, c.first_name, c.last_name, o.customer_id
+FROM orders o
+join customers c ON o.customer_id = c.customer_id
+-- we used alias with a char infront of table names and related to other columns

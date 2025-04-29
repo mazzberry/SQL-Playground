bf22a5d14499783d708f99a89c4ad31bdc811c25
Mazzberry <h.mohammadreza736@gmail.com>
2025-04-29 15:31:09 +0330

    Refactor SQL query to select specific columns from order_items, orders, and products

--- 2.Part/5_MultiJoins/multiJoins.sql
+++ 2.Part/5_MultiJoins/multiJoins.sql
@@ -5,7 +5,13 @@ JOIN customers cu ON c.customer_id = cu.customer_id
 
 
 --
-SELECT *
+SELECT 
+    oi.order_id,
+    name,
+    oi.quantity,
+    p.unit_price,
+    oi.unit_price AS p_unit_price
 FROM order_items oi
 JOIN orders o ON oi.order_id = o.order_id
 JOIN products p ON oi.product_id = p.product_id
+ORDER BY order_id DESC

f0cb5678e4d27bc3564d6011dd422d28193ec487
Mazzberry <h.mohammadreza736@gmail.com>
2025-04-29 16:21:40 +0330

    Refactor SQL query to use RIGHT JOIN for comments and products

--- 2.Part/6_OUTER_JOIN/OUTER_JOIN.sql
+++ 2.Part/6_OUTER_JOIN/OUTER_JOIN.sql
@@ -3,4 +3,16 @@ SELECT
     c.body,
     p.product_id
 FROM comments c
-JOIN products p ON c.product_id = p.product_id
+RIGHT JOIN products p ON c.product_id = p.product_id
+ORDER BY p.product_id 
+
+SELECT 
+    p.name,
+    c.body,
+    p.product_id
+FROM products p
+LEFT JOIN comments c ON c.product_id = p.product_id
+ORDER BY p.product_id 
+
+
+-- i wanna see everything in the table even if there is no comment for the product

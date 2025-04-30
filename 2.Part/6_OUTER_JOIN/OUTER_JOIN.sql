830f43062aeb4032947491c12a87c8f76540a09e
Mazzberry <h.mohammadreza736@gmail.com>
2025-04-30 08:58:29 +0330

    Add SQL query to join customers with comments for comprehensive data retrieval

--- 2.Part/6_OUTER_JOIN/OUTER_JOIN.sql
+++ 2.Part/6_OUTER_JOIN/OUTER_JOIN.sql
@@ -13,6 +13,13 @@ SELECT
 FROM products p
 LEFT JOIN comments c ON c.product_id = p.product_id
 ORDER BY p.product_id 
-
-
 -- i wanna see everything in the table even if there is no comment for the product.
+
+
+SELECT 
+    cu.customer_id,
+    cu.first_name,
+    cu.last_name,
+    co.body
+FROM customers cu 
+LEFT JOIN comments co ON cu.customer_id = co.customer_id

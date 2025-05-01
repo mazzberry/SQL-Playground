900e7ab38db1d5e31c72c8cea053e95c1be9febe
Mazzberry <h.mohammadreza736@gmail.com>
2025-05-01 18:35:09 +0330

    Fix SQL syntax by adding a semicolon to the customers join statement

--- 2.Part/5_MultiJoins/multiJoins.sql
+++ 2.Part/5_MultiJoins/multiJoins.sql
@@ -1,7 +1,8 @@
+USE store;
 SELECT p.name, cu.first_name, cu.last_name, c.body
 FROM store.comments c
 JOIN products p  ON c.product_id = p.product_id
-JOIN customers cu ON c.customer_id = cu.customer_id
+JOIN customers cu ON c.customer_id = cu.customer_id;
 
 
 --

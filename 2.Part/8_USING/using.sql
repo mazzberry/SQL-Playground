1415d8e3ab681e838aeb84d6f2640001f1b27702
Mazzberry <h.mohammadreza736@gmail.com>
2025-05-05 16:42:32 +0330

    Add using.sql for selecting orders and customers using USING clause

---
+++ 2.Part/8_USING/using.sql
@@ -0,0 +1,13 @@
+use store;
+SELECT
+	o.order_id,
+    c.first_name,
+    c.last_name
+FROM orders o 
+JOIN customers c
+USING (customer_id)
+ORDER BY o.order_id
+
+-- be jaye ON oi.product_id = p.product_id , zamanni ke
+-- product_id dar do table mojood bashad, be jaye inke
+-- ON bzanid, mitavanid az USING estefade konid

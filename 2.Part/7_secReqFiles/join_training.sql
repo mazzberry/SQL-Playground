19911ef03258ab70d4f95a8c78e2bbc422ee1bf0
Mazzberry <h.mohammadreza736@gmail.com>
2025-05-05 16:32:37 +0330

    Change LEFT JOIN to RIGHT JOIN for orders and adjust packagings join to LEFT JOIN in join_training.sql

--- 2.Part/7_secReqFiles/join_training.sql
+++ 2.Part/7_secReqFiles/join_training.sql
@@ -1,7 +1,7 @@
 use store;
 SELECT c.customer_id, o.order_id, p.name
 FROM customers c 
-RIGHT JOIN orders o
+LEFT JOIN orders o
     ON c.customer_id = o.customer_id
-JOIN packagings p 
+LEFT JOIN packagings p 
 ON p.packaging_id = o.packaging_id
